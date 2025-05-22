89e928bf3f4f4c78be0d00e4a53872186449dc9d
Lovelesh-rathore <rathorelovlesh@gmail.com>
2025-05-22 19:17:36 +0530

    Jokes Api done!!

--- Assignments/Registration form/app.js
+++ Assignments/Registration form/app.js
@@ -11,6 +11,13 @@ function ValidityState() {
   const ph = document.getElementById("phone").value.trim();
   const dob = document.getElementById("dob").value.trim();
   const per = document.getElementById("percent").value.trim();
+  const add = document.getElementById("address").value.trim();
+  const city = document.getElementById("city").value.trim();
+  const pin = document.getElementById("pincode").value.trim();
+  const guardName = document.getElementById("guardName").value.trim();
+  const guardPhoneNo = document.getElementById("guardPhoneNo").value.trim();
+  const specReq = document.getElementById("specReq").value.trim();
+
 
   //   document
   //     .querySelectorAll(".error")
@@ -42,6 +49,42 @@ function ValidityState() {
   if (!/^[0-100]+$/.test(per)){
     document.getElementById("perError").innerText=
     "Please approximate to the nearest digit & don't write percent"
+    isValid = false;
+  }
+
+  if (!/^[A-Za-z0-9 ]+$/.test(add)) {
+    document.getElementById("addressError").innerText =
+      "Please enter the valid address";
+    isValid = false;
+  }
+
+  if (!/^[A-Za-z ]+$/.test(city)) {
+    document.getElementById("cityError").innerText =
+      "Please enter your city name";
+    isValid = false;
+  }
+
+  if (!/^[0-9]+$/.test(pin)) {
+    document.getElementById("pincodeError").innerText =
+      "Please enter valid pincode";
+    isValid = false;
+  }
+
+  if (!/^[A-Za-z ]+$/.test(guardName) || nm.length < 3) {
+    document.getElementById("guardError").innerText =
+      "Please Use only Alphabets and atlest 3 characters";
+    isValid = false;
+  }
+
+  if (!/^[0-9]+$/.test(guardPhoneNo) || ph.length!=10){
+    document.getElementById("guardTelError").innerText=
+    "Please enter your 10 digit valid number"
+    isValid = false;
+  }
+
+  if (!/^[A-Za-z0-9 ]+$/.test(specReq)) {
+    document.getElementById("specReqError").innerText =
+      "Please enter valid information";
     isValid = false;
   }
 
--- Assignments/eventListner/script.js
+++ Assignments/eventListner/script.js
@@ -1,27 +1,23 @@
-// function on(){
-//     const bulb = document.getElementById("bulb");
-//     bulb.style.backgroundColor="yellow";
-// }
+function on(){
+    const bulb = document.getElementById("bulb");
+    bulb.style.backgroundColor="yellow";
+}
 
-// function off(){
-//     const bulb = document.getElementById("bulb");
-//     bulb.style.backgroundColor="white";
-// }
+function off(){
+    const bulb = document.getElementById("bulb");
+    bulb.style.backgroundColor="white";
+}
 
-// function blue(){
-//     const bulb = document.getElementById("bulb");
-//     bulb.style.backgroundColor="blue";
-// }
+function blue(){
+    const bulb = document.getElementById("bulb");
+    bulb.style.backgroundColor="blue";
+}
 
-// function green(){
-//     const bulb = document.getElementById("bulb");
-//     bulb.style.backgroundColor="green";
-// }
+function green(){
+    const bulb = document.getElementById("bulb");
+    bulb.style.backgroundColor="green";
+}
 
-// function bulbOnOff(){
-//     const bulb = document.getElementById("bulb");
-//     bulb.classList.toggle("glow");
-// }
 
 const bulb = document.querySelector("#bulb");
 const onOff = document.querySelector("#onOff");
@@ -30,14 +26,6 @@ onOff.addEventListener("click",()=>{
     bulb.classList.toggle("glow");
 })
 
-
-
-// const bulb = document.querySelector("#bulb");
-// const yellow = document.querySelector("#yellow");
-
-// bulb.addEventListener("click" , ()=>{
-
-// });
 
 
 
